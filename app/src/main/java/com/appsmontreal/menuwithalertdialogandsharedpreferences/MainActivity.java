41c02c369a1b0f8d2ee9afe5e0293ed9e00413ca
Marlon <escobar.marlon@gmail.com>
2019-10-07 10:49:15 -0400

    First commit

---
+++ app/src/main/java/com/appsmontreal/menuwithalertdialogandsharedpreferences/MainActivity.java
@@ -0,0 +1,48 @@
+package com.appsmontreal.menuwithalertdialogandsharedpreferences;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.profile:
+                Log.i("----------------->", "you pressed profile");
+                return true;
+
+            case R.id.settings:
+                Log.i("----------------->", "you pressed settings");
+                return true;
+
+            case R.id.exit:
+                finish();
+
+            default:
+                return false;
+        }
+
+    }
+}
